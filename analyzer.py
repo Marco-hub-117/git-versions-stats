9884d3d23bfc9bec189ab9bbf0c148078efa906c
Marco-hub-117 <martellotta.marco@gmail.com>
2022-02-12 17:55:01 +0100

    old code removed from analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -62,38 +62,6 @@ def copy_all_committed_file(repDirectory='.',outDirectory='analyzerOutput'):
     return True
 
 def temp_func(workdir='.'):
-    os.chdir(workdir) # Passo alla directory che contiene il repository
-
-    # Con la seguente linea di codice chiamo il comando "git reflog".
-    # l'output del comando su console sarà salvato in completedProc.stdout
-    completedProc = subprocess.run(['git', 'reflog'], capture_output = True,text = True)
-
-    hashList = [] # inizializzo la lista che conterrà tutti gli hash commit
-    for reflogLine in completedProc.stdout.splitlines():
-        # Ottengo in una lista tutte le parole restituite in una linea dal comando git reflog
-        # La prima parola (wordsList[0]) conterrà l´hash del commit
-        wordsList = reflogLine.split(' ')
-        hashList.append(wordsList[0])
-
-    count = 0 # count --> variabile utile per creare nomi dei file
-    comando = ['git', 'diff-tree','--no-commit-id','--name-only', '-r', ''] # l'ultimo elemento conterrà il commit hash
-    for eachHash in hashList:
-        comando[5] = eachHash
-        # chiamo il comando git che mi restituisce tutti i nomi dei file modificati nel dato commit hash
-        # il risultato viene salvato in completedProc.stdout
-        completedProc = subprocess.run(comando, capture_output = True, text = True)
-        fileNameList = [] # la lista conterrà tutti i nomi dei file modificati nel dato commit hash
-        for fileName in completedProc.stdout.splitlines():
-            fileNameList.append(fileName)
-
-        for fileName in fileNameList:	# per tutti i nomi dei file trovati sopra
-            if (fileName[-2:] == '.c'): # controllo che il nome del file termini in '.c'
-                            # con il seguente comando git ottengo il contenuto del file nel particolare commit hash specificato
-                            # redirigo l'output su un file .c nella directory fileCompilati
-                            # il nome del file ottenuto sarà program-{count}.c , con count che indica quanto un file è meno recente
-                            # -0 indica il file ottenuto con l'ultimo commit, mentre andando avanti avrò i file dei commit meno recenti
-                os.system(f'git show {eachHash}:{fileName} > fileCompilati/program-{count}.c')
-                count += 1
 
     os.chdir('./fileCompilati')
     fileList = os.listdir('./') # ottengo i nomi di tutti i file ottenuti grazie ai comandi git sopra
