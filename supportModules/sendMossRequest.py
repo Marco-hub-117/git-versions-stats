50653f97d5b3592f725af550e2512ee1dcfba1a8
Marco-hub-117 <martellotta.marco@gmail.com>
2022-03-21 18:06:24 +0100

    new module used to send a moss request usign mosspy

---
+++ supportModules/sendMossRequest.py
@@ -0,0 +1,59 @@
+from threading import Thread
+import time
+import mosspy
+
+userid = 719337169
+
+class MossHandlingThread(Thread):
+
+    def __init__(self, firstFileList, secondFileList, lang = 'c'):
+        Thread.__init__(self)
+        self.firstFileList = firstFileList
+        self.secondFileList = secondFileList
+        self.lang = lang
+        self.result_url = None
+
+    def run(self):
+        self.result_url = init_moss_and_send(self.firstFileList, self.secondFileList, self.lang)
+
+def init_moss_and_send(firstFileList, secondFileList, lang = 'c'):
+    """
+        Init a mosspy.Moss object and send the comparison between
+        all file contained in firstFileList and secondoFileList.
+        return the result url
+    """
+    retries = 10
+    timeout = 5 # timeout entity in seconds
+    while True:
+        m = mosspy.Moss(userid, lang)
+        m.setIgnoreLimit(420)
+        m.setNumberOfMatchingFiles(1000)
+
+        for file in firstFileList:
+            m.addFile(file)
+        for file in secondFileList:
+            m.addFile(file)
+
+        try:
+            result_url = m.send(lambda file_path, display_name: print('*', end='', flush=True))
+            break
+        except:
+            # Questo blocco l´ho aggiunto perchè mi ha dato l'errore:
+            # ConnectionResetError: [Errno 104] Connection reset by peer
+            # Ma solo ogni tanto lo fa, non so come risolvere
+            retries -= 1
+            result_url = None
+            time.sleep(5.0)
+            if retries < 0:
+                print("Errore nell'invio del gruppo:", firstFileList[0], secondFileList[0], 'ANNULLO')
+                break
+            else:
+                print("Errore nell'invio del gruppo:", firstFileList[0], secondFileList[0], 'RIPROVO')
+
+    return result_url
+
+def main ():
+    pass
+
+if __name__ == '__main__':
+    main()
