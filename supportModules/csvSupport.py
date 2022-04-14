5e461c2e1e49379fac624814925519ad275a1087
Marco-hub-117 <martellotta.marco@gmail.com>
2022-04-14 13:14:09 +0200

    list_all_rows_from_csv function added

--- supportModules/csvSupport.py
+++ supportModules/csvSupport.py
@@ -48,6 +48,21 @@ def add_rows_to_csv(csvFileName, rows):
         for row in rows:
             writer.writerow(row)
 
+def list_all_rows_from_csv(csvFileName):
+    """
+        return a list with all rows contained into csvFileName
+    """
+    with open(csvFileName, 'r', newline = '') as csvfile:
+        reader = csv.reader(csvfile)
+        field = next(reader)
+        retrievedRowsList = []
+        for row in reader:
+            rowList = []
+            for elem in row:
+                rowList.append(elem)
+            retrievedRowsList.append(rowList)
+
+        return field, retrievedRowsList
 
 def main():
     pass
